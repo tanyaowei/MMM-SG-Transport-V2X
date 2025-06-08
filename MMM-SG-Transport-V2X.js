38160257daa647b3ac4ce778813426fd1f930a64
blazex47 <blazex.net@hotmail.com>
2025-06-08 16:25:50 +0800

    Update to use mustache instead of dynamic creation

--- MMM-SG-Transport-V2X.js
+++ MMM-SG-Transport-V2X.js
@@ -60,136 +60,74 @@ Module.register("MMM-SG-Transport-V2X", {
         ]
     },
 
-    createBusStopLabelRow: function(label) {
-        var row = document.createElement("tr");
-        row.classList.add("sg-transport-bus-stop-label-row");
-        var element = document.createElement("th");
-        element.classList.add("sg-transport-bus-stop-label");
-        element.setAttribute("colspan", 4);
-        element.innerHTML = label;
-        row.appendChild(element);
-        return row;
-    },
-
-    createBusRow: function(bus) {
-
-        ArrivalTimeArray = [];
-        CrowdingLevelArray = []
-        if (bus.NextBus.EstimatedArrival !== "") {
-            ArrivalTimeArray.push(bus.NextBus.EstimatedArrival);
-            CrowdingLevelArray.push(bus.NextBus.Load)
-        }
-        if (bus.NextBus2.EstimatedArrival !== "") {
-            ArrivalTimeArray.push(bus.NextBus2.EstimatedArrival);
-            CrowdingLevelArray.push(bus.NextBus.Load)
-        }
-        if (bus.NextBus3.EstimatedArrival !== "") {
-            ArrivalTimeArray.push(bus.NextBus3.EstimatedArrival);
-            CrowdingLevelArray.push(bus.NextBus.Load)
-        }
-
-        // Don't display if no data
-        if (ArrivalTimeArray.length == 0) {
-	        return document.createElement("div");
-        }
-
-        var row = document.createElement("tr");
-        var element = document.createElement("td");
-        element.innerHTML = bus.ServiceNo + ":&nbsp;"
-        row.appendChild(element);
-
-        for (var i = 0; i < ArrivalTimeArray.length; i++) {
-            var ArrivalTime = ArrivalTimeArray[i];
-            var CrowdingLevel = CrowdingLevelArray[i];
-
-            var time = new Date(ArrivalTime);
-            var now = new Date();
-            var arrivalTimeInMinutes = Math.floor((time - now) / (60 * 1000));
-            if (arrivalTimeInMinutes < 1) {
-                arrivalTimeInMinutes = "Arr"
-            } else {
-                arrivalTimeInMinutes += "m&nbsp;"
-            }
-
-            var element = document.createElement("td");
-            element.innerHTML = arrivalTimeInMinutes + ":&nbsp;";
-            
-            // Location of stickman image
-            var pathToImage = "modules/MMM-SG-Transport-V2X/images/stickman.png";
-            // Height of image
-            var height = 17;
-
-            var image = "<img src='" + pathToImage + "' height='" + height + "'>"
-        
-            // Seating available
-            if (CrowdingLevel == "SEA") {
-                // print once
-                element.innerHTML += image;
-                
-            // Standing available
-            } else if (CrowdingLevel == "SDA") {
-                element.innerHTML += image;
-                element.innerHTML += image;
-
-            // Limited standing
-            } else if (CrowdingLevel == "LSD") {
-                element.innerHTML += image;
-                element.innerHTML += image;
-                element.innerHTML += image;
-            }
-                        
-            row.appendChild(element);
-        };
-
-        return row;
-    },
-
-    createTextRow: function(text) {
-        var row = document.createElement("tr");
-        var element = document.createElement("td");
-        element.innerHTML = text;
-        row.appendChild(element);
-        return row;
+    getScripts: function () {
+        return ["https://cdn.jsdelivr.net/npm/mustache@4.2.0/mustache.min.js"];
     },
 
     // Run on display refresh
     getDom: function() {
         // Display data
-        var wrapper = document.createElement("div");
+        const wrapper = document.createElement("div");
         wrapper.classList.add("small");
 
-        var table = document.createElement("table");
-        for (var bus_stop_id in this.bus_stops) {
-
-            var self = this;
-            let busNumbers = this.bus_stops[bus_stop_id].BusNumbers;
-            table.appendChild(this.createBusStopLabelRow(this.bus_stops[bus_stop_id].Name));
-
-            var services = this.bus_stops[bus_stop_id].Services;
-
-            if (services == null) {
-                table.appendChild(this.createTextRow("Waiting for update..."));
-            } else {
-                var services_exist = false; // flag for services with no more arrivals
-                
-                services.forEach(function(bus) {
-                    if (busNumbers == null || busNumbers.length == 0) {
-                        table.appendChild(self.createBusRow(bus));
-                        services_exist = true;
-                    } else if (busNumbers.includes(bus.ServiceNo)) {
-                        table.appendChild(self.createBusRow(bus));
-                        services_exist = true;
-                    }
-                });
-                
-                // If there are no more arrivals for bus services that we want,
-                // then the previous block should not have set services_exist to true
-                if (!services_exist) {
-                    table.appendChild(this.createTextRow("No more buses"));
-                }
-            }
+        if (!this.templateLoaded) {
+            fetch("modules/MMM-SG-Transport-V2X/public/bus_template.html")
+            .then(res => res.text())
+            .then(html => {
+                const temp = document.createElement("div");
+                temp.innerHTML = html;
+                this.template = temp.querySelector("#bus-template").innerHTML;
+                this.templateLoaded = true;
+                this.updateDom();
+            });
+            wrapper.innerHTML = "Loading template...";
+            return wrapper;
         }
-        wrapper.appendChild(table);
+
+        const now = new Date();
+
+        const data = {
+            bus_stops: Object.entries(this.bus_stops || {}).map(([code, stop]) => {
+            const services = (stop.Services || [])
+                .filter(service => {
+                const busNumbers = stop.BusNumbers;
+                return !busNumbers || busNumbers.includes(service.ServiceNo);
+                })
+                .map(service => {
+                const arrivals = [service.NextBus, service.NextBus2, service.NextBus3]
+                    .filter(bus => bus && bus.EstimatedArrival && bus.EstimatedArrival !== "")
+                    .map(bus => {
+                    const etaTime = new Date(bus.EstimatedArrival);
+                    let etaMinutes = Math.floor((etaTime - now) / 60000);
+                    etaMinutes = etaMinutes < 1 ? "Arr" : `${etaMinutes}m`;
+
+                    let stickmen = 0;
+                    switch (bus.Load) {
+                        case "SDA": stickmen = 2; break;
+                        case "LSD": stickmen = 3; break;
+                        case "SEA": stickmen = 1; break;
+                    }
+
+                    return {
+                        eta: etaMinutes,
+                        stickmen: Array.from({ length: stickmen })
+                    };
+                    });
+
+                return {
+                    serviceNo: service.ServiceNo,
+                    arrivals
+                };
+                });
+
+            return {
+                name: stop.Name,
+                services
+            };
+            })
+        };
+
+        wrapper.innerHTML = Mustache.render(this.template, data);
         return wrapper;
     },
 
